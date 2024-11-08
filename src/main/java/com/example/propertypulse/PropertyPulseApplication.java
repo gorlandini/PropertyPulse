3f5fb92c4a44a13ee88dce912e5938dd3f0cded6
guilherme orlandini <guilherme.orlandini@gmail.com>
2024-11-08 12:00:48 -0300

    First Commit

---
+++ src/main/java/com/example/propertypulse/PropertyPulseApplication.java
@@ -0,0 +1,13 @@
+package com.example.propertypulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyPulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/propertypulse/PropertyPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.propertypulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
